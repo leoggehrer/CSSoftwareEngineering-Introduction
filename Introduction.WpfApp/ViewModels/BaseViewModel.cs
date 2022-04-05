2641dc7e0cda1a45dc9f1c05ae12941a74c79293
Gehrer Gerhard <56598504+leoggehrer@users.noreply.github.com>
2022-04-05 18:54:05 +0200

    Add project files.

---
+++ Introduction.WpfApp/ViewModels/BaseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace Introduction.WpfApp.ViewModels
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
